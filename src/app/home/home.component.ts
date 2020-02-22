f93e4415b3b896d9c64a168ccccb45ce5ed5abb9
Ankit Pant <ankit.pant@klm.com>
2020-02-22 03:36:36 +0100

    added employee details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  GetEmployeesGQL,
+  GetEmployeesQuery
+} from '@core/graphql/types.angularApollo';
+import { pluck, takeUntil } from 'rxjs/operators';
+import { Subject, Observable } from 'rxjs';
+import { ApolloQueryResult } from 'apollo-client';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +13,31 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  constructor() {}
+  public employeeDetails: GetEmployeesQuery = {
+    employee: []
+  };
 
-  ngOnInit() {}
+  private employeeDetails$: Observable<ApolloQueryResult<GetEmployeesQuery>>;
+  private destroy$: Subject<boolean> = new Subject<boolean>();
 
-  ngOnDestroy() {}
+  constructor(public getEmployeesGQL: GetEmployeesGQL) {}
+
+  ngOnInit() {
+    this.getEmployeeDetails();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
+  }
+
+  private getEmployeeDetails() {
+    this.employeeDetails$ = this.getEmployeesGQL
+      .watch()
+      .valueChanges.pipe(takeUntil(this.destroy$));
+
+    this.employeeDetails$.pipe(pluck('data')).subscribe(response => {
+      this.employeeDetails.employee = response.employee;
+    });
+  }
 }
